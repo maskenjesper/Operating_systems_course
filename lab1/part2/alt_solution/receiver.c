2de0b2a890a9bb55d8046b6e157da66ced3505ef
jakobgotberg <jakob.gotberg@gmail.com>
2022-11-06 19:38:37 +0100

    missed to do the last part

--- lab1/part2/alt_solution/receiver.c
+++ lab1/part2/alt_solution/receiver.c
@@ -1,10 +1,20 @@
 #include <mqueue.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 
+int count_words(char* buffer, int length)
+{
+	int i;
+	int words = 0;
+	for(i = 0; i < length-2; i++)
+	{
+	    if( (buffer[i] != ' ' && buffer[i+1] == ' ' && buffer[i+2] != ' ') || (buffer[i+2] == '\n' && i != length-3) )
+		words++;
+	}
+	return ++words;
+	
+}
 void arg_error(void)
 {
     const char *error_msg = "argv[1] = Name of message queue.\n";
@@ -24,7 +34,7 @@ void error_output(char* msg)
 int main(int argc, char** argv)
 {
 	if(argc != 2)
-		arg_error();
+	    arg_error();
 
 	const char *queue_name = argv[1];
 	mqd_t mqd;	
@@ -34,23 +44,22 @@ int main(int argc, char** argv)
 
 	mqd = mq_open(queue_name, flags);
 	if(mqd == -1)
-		error_output("can not open queue\n");
+	    error_output("can not open queue\n");
 
 	if(mq_getattr(mqd, &attr) == -1)
-		error_output("can not get attributes\n");
+	    error_output("can not get attributes\n");
 
 	read_buffer = malloc(attr.mq_msgsize);
 	if(read_buffer == NULL)
-		error_output("the malloc failed\n");
+	    error_output("the malloc failed\n");
 
 	ssize_t bytes_read = mq_receive(mqd, read_buffer, attr.mq_msgsize, 0);
 	if(bytes_read == -1)
-		error_output("mq_receive failed\n");
-
-	write(STDOUT_FILENO, read_buffer, bytes_read);
+	    error_output("mq_receive failed\n");
 
 	if(mq_unlink(queue_name) == -1)
-    	error_output("can not unlink\n");
+    	    error_output("can not unlink\n");
 
+	printf("words: %d\n", count_words(read_buffer, bytes_read));
 	exit(EXIT_SUCCESS);
 }
--- lab1/part2/alt_solution/sender.c
+++ lab1/part2/alt_solution/sender.c
@@ -1,9 +1,7 @@
-#include <sys/stat.h>
 #include <mqueue.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
 
 void arg_error(void)
@@ -27,29 +25,29 @@ void error_output(char* msg)
 int main(int argc, char** argv)
 {
 
-	if(argc != 4)
-		arg_error();
+    if(argc != 4)
+    arg_error();
 
-	const char *text_name = argv[1];
+    const char *text_name = argv[1];
     int text_size = atoi(argv[2]);
     const char *mq_name = argv[3];
 
-	char buffer[text_size];
-	int fd = open(text_name, O_RDONLY);
+    char buffer[text_size];
+    int fd = open(text_name, O_RDONLY);
     ssize_t read_bytes = read(fd, buffer, text_size);
 
     mqd_t mqd;
     int flags = O_RDWR | O_CREAT;
-	mode_t perm = 777;
-	struct mq_attr *attr_ptr;
-	attr_ptr = NULL;
+    mode_t perm = 777;
+    struct mq_attr *attr_ptr;
+    attr_ptr = NULL;
 
     mqd = mq_open(mq_name, flags, perm, attr_ptr);
     if(mqd == -1)
-		error_output("can not open message queue\n");	
+        error_output("can not open message queue\n");	
 
     if(mq_send(mqd, buffer, read_bytes, 0) == -1)
-		error_output("can not send msg\n");
+        error_output("can not send msg\n");
 
     exit(EXIT_SUCCESS);
 }
