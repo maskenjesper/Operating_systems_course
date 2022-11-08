886b95c27c65f380cc461da0bacaec5e0620ef0e
Jakob Olsson <jakobolsson973@gmail.com>
2022-11-08 14:54:19 +0100

    Fixed warnings in part1

--- lab1/part1/main.c
+++ lab1/part1/main.c
@@ -1,5 +1,6 @@
 // Program to illustrate the command ls / | wc -l
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
 #include "../../utilities.c"
@@ -9,31 +10,28 @@ int main() {
     if (pipe(fd) == -1) 
         errExit("pipe");
 
-    switch (fork()) {
-        case -1:
-            errExit("fork");
-
-        case 0: // child
-            if (close(fd[0]) == -1) // close unused read end
-                errExit("close");
-            dup2(fd[1], STDOUT_FILENO); // change starnard output to pipe
-            if (close(fd[1]) == -1) // close unused write end
-                errExit("close");
-            const char *argsc[] = {"ls", "/", NULL}; // create array of arguments
-            if (execv("/bin/ls", argsc) == -1) // replace process with ls program
-                errExit("execv");
-            break;
-
-        default: // parent
-            if (close(fd[1]) == -1) // close unused write end
-                errExit("close");
-            dup2(fd[0], STDIN_FILENO); // change starnard input to pipe
-            if (close(fd[0]) == -1) // close unused read end
-                errExit("close");
-            wait(NULL); // wait for child process to finish
-            const char *argsp[] = {"wc", "-l", NULL}; // create array of arguments
-            if (execv("/bin/wc", argsp) == -1) // replace process with wc program
-                errExit("execv");
-            break;
+    pid_t pid = fork();
+    if (pid == -1) 
+        errExit("fork");
+    else if (pid == 0) { // child
+        if (close(fd[0]) == -1) // close unused read end
+            errExit("close");
+        dup2(fd[1], STDOUT_FILENO); // change starnard output to pipe
+        if (close(fd[1]) == -1) // close unused write end
+            errExit("close");
+        char *const args[] = {"ls", "/", NULL}; // create array of arguments
+        if (execv("/bin/ls", args) == -1) // replace process with ls program
+            errExit("execv");
+    }
+    else { // parent
+        if (close(fd[1]) == -1) // close unused write end
+            errExit("close");
+        dup2(fd[0], STDIN_FILENO); // change starnard input to pipe
+        if (close(fd[0]) == -1) // close unused read end
+            errExit("close");
+        wait(NULL); // wait for child process to finish
+        char *const args[] = {"wc", "-l", NULL}; // create array of arguments
+        if (execv("/bin/wc", args) == -1) // replace process with wc program
+            errExit("execv");
     }
 }
