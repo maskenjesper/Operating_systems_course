6d5d032e4ab0fc4ab08c80245c702c23ef3c86d5
jakobgotberg <jakob.gotberg@gmail.com>
2022-11-24 15:38:34 +0100

    TID

--- assignments/os_assignment2_group_9001/gotberg/main.c
+++ assignments/os_assignment2_group_9001/gotberg/main.c
@@ -12,7 +12,7 @@ runner (void *counter)
     pthread_mutex_lock(&lock);
     if(buffer < MAX_VALUE)
     {
-        printf("%ld:\tpid: %d\tbuffer:%d\n", (long) pthread_self(), getpid(), buffer++);
+        printf("TID: %ld PID: %d buffer: %d\n", (long) pthread_self(), getpid(), buffer++);
         pthread_mutex_unlock(&lock);
         *counter_ptr += 1;
         runner(counter);
